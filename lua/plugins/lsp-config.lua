5786c0b663bbc4b38e1ec97a078c23c129920738
Thiago Koster <thiagokoster@gmail.com>
2024-02-24 19:12:06 +0100

    add autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,9 +16,11 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.gopls.setup({})
+      lspconfig.lua_ls.setup({capabilities = capabilities})
+      lspconfig.gopls.setup({capabilities = capabilities})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
