bf654ca35c3144263c983f158ef0f5cdedb64f3c
Thiago Koster <thiagokoster@gmail.com>
2024-02-24 11:45:33 +0100

    repo init

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require('nvim-treesitter.configs')
+    config.setup {
+      -- Add languages to be installed here that you want installed for treesitter
+      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'rust'},
+
+      -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+      auto_install = false,
+      -- Install languages synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- List of parsers to ignore installing
+      ignore_install = {},
+      -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+      highlight = { enable = true },
+      indent = { enable = true },
+    }
+  end
+}
