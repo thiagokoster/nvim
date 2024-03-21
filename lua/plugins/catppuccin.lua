e2073b8b2d06fe857ce9b7f356c2b60c71aab351
Thiago Koster Lago <thiagokoster@gmail.com>
2024-03-21 16:34:08 +0100

    change theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,10 @@ return {
   name = "catppuccin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      flavour = "frappe",
+      transparent_background = true,
+    })
     vim.cmd.colorscheme "catppuccin"
   end
 }
